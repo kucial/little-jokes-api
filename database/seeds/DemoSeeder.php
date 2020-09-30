470900f644d8a6b190af6fa752bfa42f8dc73420
kongkx <kongkx.yang@gmail.com>
2020-09-30 11:15:29 +0800

    add demo seeder

---
+++ database/seeds/DemoSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DemoSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $rawStr = file_get_contents(__DIR__. '/demo.json');
+        $data = json_decode($rawStr, true);
+        \App\User::truncate();
+        // user
+        factory(\App\User::class, 20)->create();
+
+        // jokes
+        \App\Post::truncate();
+        foreach ($data['jokes'] as $item) {
+            $post = new \App\Post();
+            $post->content = $item;
+            $post->save();
+        }
+    }
+}
