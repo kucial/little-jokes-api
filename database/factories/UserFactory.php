30620303497185232c6d9469c8e8c3bcc8e9b628
kongkx <kongkx.yang@gmail.com>
2022-01-26 21:28:27 +0800

    fix UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@
 use App\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,12 +18,18 @@ use Illuminate\Support\Str;
 |
 */
 
+
+
 $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'mobile' => "+86" . $faker->unique()->phoneNumber,
+        'mobile_verified_at' => now(),
+        'password' => 'password', // password
+        'region' => 'CN',
         'remember_token' => Str::random(10),
+        'api_token' => User::generateToken(),
     ];
 });
